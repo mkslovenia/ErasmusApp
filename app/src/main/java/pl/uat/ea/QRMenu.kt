f6fb6005e31475ffa0f9bf88de27b8a96532cb2e
mkslovenia <marcin.krzyszkowski@student.um.si>
2021-06-15 21:18:17 +0200

    creating GUI and fragments for each tab

---
+++ app/src/main/java/pl/uat/ea/QRMenu.kt
@@ -0,0 +1,12 @@
+package pl.uat.ea
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class QRMenu : Fragment() {
+
+
+}
---
+++ app/src/main/java/pl/uat/ea/Weather.kt
@@ -0,0 +1,11 @@
+package pl.uat.ea
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Weather : Fragment() {
+   
+}
